ba7abda9e0e3b06de6c0fb05dffa6e0da4c34567
unknown <elstermetalhead@gmail.com>
2025-03-24 18:34:22 +0100

    poprawiono footer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -608,6 +608,7 @@ function Footer() {
   return (
     <footer>
       <p>&copy; 2025 Ivan Sarokin, NIP: 8992832244. Wszelkie prawa zastrzeżone.</p>
+      <p>E-mail: kontakt@sorokokorki.com.pl</p>
     </footer>
   );
 }
